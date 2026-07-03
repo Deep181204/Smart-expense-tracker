285ad0d337b9e1d348bc56d1c1b5cb01e8e297d5
Deep181204 <deepahahh18@gmail.com>
2026-07-03 10:29:16 +0530

    Organized frontend structure

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Dashboard from "../pages/Dashboard";
+import Income from "../pages/Income";
+import Expenses from "../pages/Expenses";
+import Profile from "../pages/Profile";
+import NotFound from "../pages/NotFound";
+
+function AppRoutes() {
+  return (
+    <Routes>
+
+      <Route path="/" element={<Login />} />
+
+      <Route path="/signup" element={<Signup />} />
+
+      <Route path="/dashboard" element={<Dashboard />} />
+
+      <Route path="/income" element={<Income />} />
+
+      <Route path="/expenses" element={<Expenses />} />
+
+      <Route path="/profile" element={<Profile />} />
+
+      <Route path="*" element={<NotFound />} />
+
+    </Routes>
+  );
+}
+
+export default AppRoutes;
